f5e65cd6aedad6398fe705c4203645ddfc4ae6da
toon-restore <ashxen7771@gmail.com>
2021-11-18 19:45:55 +0400

    Initial commit

---
+++ src/task1/BufferdInputStream.java
@@ -0,0 +1,39 @@
+package task1;
+
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class BufferdInputStream {
+
+
+    public static void getPage(String address) throws IOException, URISyntaxException {
+        URL url = new URL(address);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Download.html"));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            writer.write(line);
+        }
+        reader.close();
+        writer.close();
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            getPage("http://www.example.com/");
+
+        } catch (Exception ex) {
+            System.out.println("ERROR");
+        }
+    }
+
+}
+
+
+
